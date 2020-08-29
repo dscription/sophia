5e1b80b46fe81ea9effd152e5cb0cd0be3489ef4
dscription <dscriptionplayground@gmail.com>
2020-08-28 23:29:46 -0400

    create topic cards

--- controllers/topics.js
+++ controllers/topics.js
@@ -1,16 +1,25 @@
 const User = require('../models/user');
 
 module.exports = {
-  create
+  create,
+  show
 };
 
+function show(req, res) {
+  console.log(req)
+  res.render('topics/index', {
+    title: 'Your Topics',
+    user: req.user,
+    topics: req.user.topics,
+  })
+}
 
 function create(req, res) {
   console.log('request information', req)
   User.findById(req.user._id, function (err, user) {
     user.topics.push(req.body);
     user.save(function (err) {
-      res.redirect(`/users/${user._id}`)
+      res.redirect(`/users/${user._id}/topics`)
     })
   })
 }
--- routes/topics.js
+++ routes/topics.js
@@ -3,5 +3,6 @@ const router = express.Router();
 const topicsCtrl = require('../controllers/topics');
 
 router.post('/users/:id/topics', topicsCtrl.create);
+router.get('/users/:id/topics', topicsCtrl.show)
 
 module.exports = router;
