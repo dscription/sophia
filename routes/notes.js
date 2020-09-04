dc90bd5365be6cf8a729986e8d4def5836dfe370
dscription <dscriptionplayground@gmail.com>
2020-09-04 08:42:35 -0400

    fix

--- routes/notes.js
+++ routes/notes.js
@@ -2,12 +2,16 @@ var express = require('express');
 var router = express.Router();
 const notesCtrl = require('../controllers/notes')
 
-router.get('/topics/:topicId/contents/:contentId/newNote', notesCtrl.new);
-router.get('/contents/notes', notesCtrl.index);
-router.post('/topics/:topicId/contents/:contentId', notesCtrl.create);
-router.get('/topic/:topicId/content/:contentId/notes/:noteId/remove', notesCtrl.delete);
-router.get('/topic/:topicId/content/:contentId/notes/:noteId/updateView', notesCtrl.updateView);
-router.put('/topic/:topicId/content/:contentId/notes/:noteId/update', notesCtrl.update);
+router.get('/topics/:topicId/contents/:contentId/newNote',isLoggedIn, notesCtrl.new);
+router.get('/contents/notes',isLoggedIn, notesCtrl.index);
+router.post('/topics/:topicId/contents/:contentId',isLoggedIn, notesCtrl.create);
+router.get('/topic/:topicId/content/:contentId/notes/:noteId/remove',isLoggedIn, notesCtrl.delete);
+router.get('/topic/:topicId/content/:contentId/notes/:noteId/updateView',isLoggedIn, notesCtrl.updateView);
+router.put('/topic/:topicId/content/:contentId/notes/:noteId/update',isLoggedIn, notesCtrl.update);
 
+function isLoggedIn(req, res, next) {
+  if (req.isAuthenticated()) return next();
+  res.redirect("/auth/google");
+}
 
 module.exports = router;
